c75c8311b2290c63399252785e83c274091e97e3
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2016-10-09 14:35:29 -0400

    Add a script by Zuris that checks your access log for nasty entries, at fullsec even!

---
+++ scripts/fullsec_check_access_log.js
@@ -0,0 +1,29 @@
+// made by Zuris.
+// the original is reportedly available as `diag.log_analyzer`.
+// this script checks sys.access_log only, in a way that's verifiable - so if you check scripts.get_level on this before running it, you'll know for sure that it doesn't touch your upgrades.
+function(context, args){ // { script }
+    if ((args === null) || (typeof args.script === 'undefined')) {
+        return {ok:false, msg:"Please call this script with {script:#s.sys.access_log} to allow access to your logs"};
+    }
+    var logfile = args.script.call();
+
+    var npc_prefix = ['abndnd', 'uknown', 'unknown', 'derelict', 'anonymous', 'unidentified', 'anon'];
+    var insert;
+    var logs = [];
+
+    logfile.map(function(val, index) {
+        insert = true;
+        for (var i = 0; i < npc_prefix.length; i++) {
+            if (val.indexOf(npc_prefix[i]) > -1) {
+                insert = false;
+            }
+        }
+        
+        if (insert) {
+            logs.push(val);
+        }
+    });
+
+    return logs;
+}
+// made by Zuris
