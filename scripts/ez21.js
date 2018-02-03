043ad2e39e90fce1ae45c30c59a234fbcca934ac
zeta <alperkocer.03102003@gmail.com>
2018-02-03 20:40:25 +0300

    tidied up ez21.js
    cleaned the code (dear god the old version was horrible), added more detailed descriptions on what the script is doing, and threw in a small scriptor-subscript difference explanation to avoid confusion

--- scripts/ez21.js
+++ scripts/ez21.js
@@ -1,24 +1,35 @@
 // Instructional script for cracking EZ_21 locks.
-// Syntax: script {t: #s.some_user.their_loc }
+// Syntax: user.ez21 {t:#s.some_user.their_loc}
 // By mushin0
+// edited by zeta
  
 function (context, args){
-    var target = args.t;                // target = #s.some_user.their_loc
-    var response = target.call({});     // response = #s.some_user.their_loc.call({})
-    var substring1 = "EZ_21";           // check var for lock type
-    var substring2 = "UNLOCKED";        // check var for unlocked
-    var i = 0;                          // counter for array
+    var target = args.t, response = target.call({}), passwords = ["unlock","open","release"]
+ 
+    /* target = #s.some.loc
+     * response = #s.some.loc.call({})
+     *
+     * IMPORTANT DIFFERENCE BETWEEN #s AND #Xs:
+     * #s.some.script is only usable through the CLI, and is an object.
+     * #Xs.some.script is only usable in scripts, and is a function call. X can be: 4/f (fullsec), 3/h (highsec), 2/m (midsec), 1/l (lowsec), 0/n (nullsec)
+     *
+     * passwords is an array containing every possible solution for the EZ_21 lock
+     *
+     */
    
-    // response.indexOf(substring) searches for substring within response and reports back
-    // an int higher than -1 if it is found, otherwise it is -1
+    // response.includes() checks if the said variable or literal, in this case response, includes a certain variable or literal.
+    // true if it includes it, false if not
+  
    
-    if ( response.indexOf(substring1) > -1 ){               // check for lock type
-    while ( response.indexOf(substring2) === -1 ) {         // check for unlocked
-        var passwords = ["unlock","open","release"];        // init array with passwords
-        var response = target.call ({EZ_21: passwords[i] });// Try passwords
-        i++;                                                // increment i for array
-    }}
+    if ( response.includes("EZ_21") ) {                            // we check if the first lock is EZ_21. if it is, we get to cracking it
+        for ( var i = 0; i < passwords.length; i++ ) {             // we use a for loop, which runs once for each element in the array "passwords"
+            response = target.call ({EZ_21: passwords[i] });       // we update the variable "response" with the output we get after trying the first password
+            if ( response.includes("`NLOCK_UNLOCKED` EZ_21") ) {   // we check if the output contains the LOCK_UNLOCKED message of the EZ_21 lock
+                break;                                             // if it does include it, we break out of the loop. if it doesnt, we try the next password
+            }
+        }
+    }
    
-    return response;
+    return response; //we return the output of the loc call
    
 }
